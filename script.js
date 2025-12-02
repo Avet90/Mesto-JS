6bd87c8285c1f1e957fc369f9b1477f37e8ac20d
Avet90 <avsar260390@gmail.com>
2025-12-02 21:09:16 +0300

    I made function addCard, deleteCard and tamplate

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const content = document.querySelector('.content');
-const element = content.querySelector('.element');
+const elementsContainer = content.querySelector('.elements-container');
 
 const addButton = content.querySelector('.profile__add-element-button');
 
 const popupOverlay = content.querySelector('.popup-overlay');
+const submitButton = popupOverlay.querySelector('.submit-btn');
 
 const closeButton = popupOverlay.querySelector('.close-btn')
 
@@ -18,6 +19,35 @@ function closePopup() {
     popupOverlay.classList.remove('active');
 }
 
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+function addCard(linkPicture, namePicture) {
+    const cardTemplate = document.querySelector('#element-template');
+    const cardElement = cardTemplate.content.querySelector('.element').cloneNode(true);
+
+
+    cardElement.querySelector('.element__title').textContent = namePicture;
+    cardElement.querySelector('.element__image').src = linkPicture;
+    cardElement.querySelector('.element__image').alt = namePicture;
+
+
+    cardElement.querySelector('.element__like').addEventListener('click', function(evt){
+        const likeElement = evt.target;
+        likeElement.classList.toggle('element__like_active');
+    })
+
+    cardElement.querySelector('.element__trash').addEventListener('click', function(evt){
+        deleteCard(cardElement);
+    })
+
+    elementsContainer.append(cardElement);
+}
+
+
+
+
 
 // EventListeners
 
@@ -26,13 +56,24 @@ addButton.addEventListener('click', openPopup)
 closeButton.addEventListener('click', closePopup)
 
 popupOverlay.addEventListener('click', function (evt) {
+
     if (evt.target === popupOverlay) {
+        closePopup();
+    }
+
+    if(evt.target === submitButton){
+        
+        const namePicture = popupOverlay.querySelector('.placeName');
+        const linkPicture = popupOverlay.querySelector('.imageUrl');
+
+        addCard(linkPicture.value, namePicture.value);
+
+        linkPicture.value = ''; 
+        namePicture.value = '';
+
         closePopup();
     }
 })
 
-element.addEventListener('click', function(evt){
-    if (evt.target.classList.contains('element__like')) {
-        evt.target.classList.toggle('element__like_active');
-    }
-})
+
+
