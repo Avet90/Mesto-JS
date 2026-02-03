66c2860ab170f363a9e918e79ea33ba363432ae1
Avet90 <avsar260390@gmail.com>
2026-02-03 18:08:37 +0300

    I created a custom error message and moved all validation to a separate file.

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const formCard = content.querySelector('#formCard')
 // Imports
 import {createCard} from './components/cards.js';
 import {openPopup, closePopup} from './components/modal.js';
+import {enableValidation} from './components/validation.js'
 
 
 // Functions
@@ -56,76 +57,6 @@ function setInputValue() {
     enableValidation();
 
 }
-
-
-
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('form__input_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('form__input-error_active');
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_type_error');
-  errorElement.classList.remove('form__input-error_active');
-  errorElement.textContent = '';
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement)=>{
-    return !inputElement.validity.valid;
-  })
-}
-
-const toggleButtonState =(inputList, buttonElement)=>{
-  if(hasInvalidInput(inputList)){
-    buttonElement.disabled = true;
-    buttonElement.classList.add('button_inactive');
-  }else{
-    buttonElement.disabled = false;
-    buttonElement.classList.remove('button_inactive');
-  }
-}
-
-const setEventListeners = (formElement) => {
-
-  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-  const buttonElement = formElement.querySelector('.form__submit');
-
-  toggleButtonState(inputList, buttonElement);
-
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.form'));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', function (evt) {
-      evt.preventDefault();
-    });
-
-    setEventListeners(formElement);
-  });
-};
-
-
-
 
 
 // Hendlers
